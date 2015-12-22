0805014cd3ba5cf4c67c6c3bcd9799181e2ba2ea
Humberto Vieira de Castro <humbertovieira.castro@gmail.com>
2015-12-22 14:38:30 -0200

    Inicialização do projeto

---
+++ MusicSearchItunes/MusicSearchItunes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicSearchItunes
+//
+//  Created by Humberto Vieira de Castro on 12/22/15.
+//  Copyright © 2015 Humberto Vieira de Castro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
