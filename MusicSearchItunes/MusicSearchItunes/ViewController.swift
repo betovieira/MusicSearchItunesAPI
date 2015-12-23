72fe78825d8485a014a0bea850387d2858eed443
Humberto Vieira de Castro <humbertovieira.castro@gmail.com>
2015-12-23 11:48:53 -0200

    comentado!

--- MusicSearchItunes/MusicSearchItunes/ViewController.swift
+++ MusicSearchItunes/MusicSearchItunes/ViewController.swift
@@ -22,32 +22,34 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         
         pageIndicator.numberOfPages = 0
         scrollView.delegate = self
-        
-        
     
     }
     @IBAction func searchForMusic(sender: AnyObject) {
         searchTextField.resignFirstResponder()
         
+        /* Se estiver o texto estiver vazio */
         if searchTextField.text != "" {
             
+            /* Chama a função de classe pra carregar a scroll view */
             ItunesConnection.getAlbumForString(searchTextField.text!) { (album:Album) -> () in
                 
+                /* Incrementa no numero de itens */
                 self.numberOfItems++
                 self.pageIndicator.numberOfPages = self.numberOfItems
                 
+                /* Altera o tamanho da scroll view dinamicamente */
                 self.scrollView.contentSize = CGSizeMake(CGFloat(self.numberOfItems) * self.scrollView.frame.size.width, self.scrollView.frame.height)
                 
-                
+                /* Inicializa a xib */
                 let musicView = NSBundle.mainBundle().loadNibNamed("MusicView", owner: self, options: nil).last as! MusicView
                 
-                
+                /*  */
                 musicView.frame = CGRectMake(CGFloat(self.numberOfItems - 1) * self.scrollView.frame.size.width, 0, self.scrollView.frame.size.width, self.scrollView.frame.size.height)
                 musicView.updateConstraints()
                 
                 
     
-                
+                /* Dispara a thread para carregar as informações */
                 dispatch_async(dispatch_get_main_queue(), { () -> Void in
                     musicView.addDataToMusicView(album)
                     self.scrollView.addSubview(musicView)
@@ -60,6 +62,7 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         }
     }
     
+    /* Evento da scroll view - Quando ela fica parada depois de um movimento */
     func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
         let page = Int(scrollView.contentOffset.x / scrollView.frame.size.width)
         pageIndicator.currentPage = page
